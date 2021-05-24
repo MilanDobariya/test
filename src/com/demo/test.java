817893673f4ce015337b67582953b31624d57dee
milan dobariya <milandobariya1996@gmail.com>
2021-05-24 15:53:29 +0530

    commit 3 dev

--- src/com/demo/test.java
+++ src/com/demo/test.java
@@ -3,6 +3,6 @@ package com.demo;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("in method commit 2");
+		System.out.println("in method commit 3 with dev branch");
 	}
 }
