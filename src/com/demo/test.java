7a833ec418a4b20dc8757937974d8f8738d66252
milan dobariya <milandobariya1996@gmail.com>
2021-07-04 08:16:01 +0530

    committed in feature-1

--- src/com/demo/test.java
+++ src/com/demo/test.java
@@ -3,9 +3,6 @@ package com.demo;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("in method commit 2");
-		//first change dev2
-		//first change dev1
-		System.out.println("in method commit 3 with dev branch");
+		System.out.println("my feature");
 	}
 }
